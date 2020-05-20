6137360cef122c364fe9e269fad91d99aa9991c2
koh97222 <koh97222@gmail.com>
2020-05-20 20:43:56 +0900

    [mod]投稿のコンポーネントを分離（仮）

---
+++ reco-share/src/components/Post.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="container">
+    <b-row>
+      <b-col xl="2"></b-col>
+      <b-col xl="8" cols="12">
+        <b-card class="post">
+          <b-row>
+            <b-col xl="2" cols="3"><b-badge>title</b-badge></b-col>
+            <b-col xl="10" cols="9"
+              ><h6 class="text-left">
+                <b-link class="text-left">最近買ったターンテーブル</b-link>
+              </h6></b-col
+            >
+          </b-row>
+          <b-row>
+            <b-col xl="2" cols="3"><b-badge>tag</b-badge></b-col>
+            <b-col xl="10" cols="9"
+              ><h6 class="text-left">
+                <b-link class="text-left">#record </b-link>
+                <b-link class="text-left">#vinyl </b-link>
+                <b-link class="text-left">#tokyo </b-link>
+              </h6></b-col
+            >
+          </b-row>
+          <b-row>
+            <b-col xl="2" cols="3"><b-badge>user</b-badge></b-col>
+            <b-col xl="10" cols="9"
+              ><h6 class="text-left">
+                <b-link class="text-left">@user</b-link>
+              </h6></b-col
+            >
+          </b-row>
+          <hr />
+          <div class="img">
+            image
+          </div>
+          <hr />
+        </b-card>
+      </b-col>
+      <b-col xl="2"></b-col>
+    </b-row>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style scoped>
+.post {
+  box-shadow: 2px 2px 4px #cccccc;
+}
+</style>
--- reco-share/src/views/Timeline.vue
+++ reco-share/src/views/Timeline.vue
@@ -2,64 +2,31 @@
   <div class="timeline">
     <div class="container">
       <div class="title">
-        <h2>FEATURED</h2>
+        <h2 class="fw-700">FEATURED</h2>
         <hr />
+        <post class="mt-5"></post>
       </div>
       <div class="latest-post title">
-        <h2>LATEST POST</h2>
+        <h2 class="fw-700">LATEST POST</h2>
         <hr />
-        <template>
-          <div class="container">
-            <b-row>
-              <b-col xl="2"></b-col>
-              <b-col xl="8" cols="12">
-                <b-card>
-                  <b-row>
-                    <b-col xl="2" cols="3"><b-badge>title</b-badge></b-col>
-                    <b-col xl="10" cols="9"
-                      ><h6 class="text-left">
-                        <b-link class="text-left"
-                          >最近買ったターンテーブル</b-link
-                        >
-                      </h6></b-col
-                    >
-                  </b-row>
-                  <b-row>
-                    <b-col xl="2" cols="3"><b-badge>tag</b-badge></b-col>
-                    <b-col xl="10" cols="9"
-                      ><h6 class="text-left">
-                        <b-link class="text-left">#record </b-link>
-                        <b-link class="text-left">#vinyl </b-link>
-                        <b-link class="text-left">#tokyo </b-link>
-                      </h6></b-col
-                    >
-                  </b-row>
-                  <b-row>
-                    <b-col xl="2" cols="3"><b-badge>user</b-badge></b-col>
-                    <b-col xl="10" cols="9"
-                      ><h6 class="text-left">
-                        <b-link class="text-left">@user</b-link>
-                      </h6></b-col
-                    >
-                  </b-row>
-                  <hr />
-                  <div class="img">
-                    image
-                  </div>
-                  <hr />
-                </b-card>
-              </b-col>
-              <b-col xl="2"></b-col>
-            </b-row>
-          </div>
-        </template>
+        <post class="mt-5"></post>
+        <post class="mt-5"></post>
+        <post class="mt-5"></post>
+        <b-button
+          class="mt-5 mb-5"
+          style="background-color:black; font-weight:400"
+          >VIEW MORE</b-button
+        >
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+import Post from "../components/Post.vue";
+export default {
+  components: { Post },
+};
 </script>
 
 <style>
@@ -70,6 +37,13 @@ export default {};
 }
 .badge-secondary {
   width: 60px;
+}
+.btn {
+  width: 300px;
+  height: 50px;
+}
+.fw-700 {
+  font-weight: 700;
 }
 .timeline {
   background-color: #f2f2f2;
